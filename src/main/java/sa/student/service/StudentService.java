b27c07cc66c246fa8e7017f76437b4030f078de3
Marlon Enrique Noguera Ramirez <menoguerar@unal.edu.co>
2018-11-05 01:06:31 -0500

    para pasar a dockerhub

---
+++ src/main/java/sa/student/service/StudentService.java
@@ -0,0 +1,48 @@
+package sa.student.service;
+
+import sa.student.model.Student;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@Stateless
+public class StudentService {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    public List<Student> getAllStudents(int first, int maxResult) {
+        return entityManager.createNamedQuery(Student.FIND_ALL)
+                .setFirstResult(first).setMaxResults(maxResult).getResultList();
+    }
+
+    public Student getStudentByCode(long code){
+        return entityManager.find(Student.class, code);
+    }
+//method to obtain studet by its username
+    public Student getStudentByUsername(String username){
+        Student studentByUsername = (Student)entityManager.createNamedQuery(Student.FIND_BY_USERNAME).getSingleResult();
+        return studentByUsername;
+    }
+
+    public Student createStudent(Student student) {
+        entityManager.persist(student);
+        entityManager.flush();
+        return student;
+    }
+
+    public Student updateStudent(long code, Student student) {
+        Student studentToUpdate = entityManager.find(Student.class, code);
+        studentToUpdate.setUsername(student.getUsername());
+        studentToUpdate.setPassword(student.getPassword());
+        entityManager.merge(studentToUpdate);
+        return entityManager.find(Student.class, code);
+    }
+
+    public long deleteStudent(long code) {
+        Student student = entityManager.find(Student.class, code);
+        entityManager.remove(student);
+        return code;
+    }
+}
